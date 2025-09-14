04bc115b91a89a417416af655b8c8f250d5d9a21
Vlad <vladimir.dubceac@snt.md>
2025-09-14 13:35:35 +0300

    Spring context

--- src/main/java/md/vladdubceac/learning/spring6di/Spring6DiApplication.java
+++ src/main/java/md/vladdubceac/learning/spring6di/Spring6DiApplication.java
@@ -1,13 +1,21 @@
 package md.vladdubceac.learning.spring6di;
 
+import md.vladdubceac.learning.spring6di.controllers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Spring6DiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Spring6DiApplication.class, args);
+		ApplicationContext context  = SpringApplication.run(Spring6DiApplication.class, args);
+
+		MyController controller = context.getBean(MyController.class);
+
+		System.out.println("In Main Method");
+
+		System.out.println(controller.sayHello());
 	}
 
 }
